f770404b3b5f7d67f22fe5eeb9c67194e576d9af
Geoffrey Matthews <97923330+YKTV8@users.noreply.github.com>
2022-08-11 22:11:23 -0700

    08/11 UPDATED
    JS coded. Zero Errors displayed in DOM.
    -Background Color(s) : pending
    -smooth scroll: pending

--- app.js
+++ app.js
@@ -1,33 +1,78 @@
 /**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
-
-/**
  * Define Global Variables
  * 
 */
-const sections = Array.from(document.querySelectorAll('sections'));
-const menu = document.getElementById("NavBar__list");
-const numberOfListItems = sections.length;
+const navBar = document.querySelector('.navbar__menu');
+const navListUL = document.querySelector('#navbar__list');
+const sections = document.querySelectorAll('section');
 
+
+//const sections = Array.from(document.querySelectorAll('sections'));
+//const menu = document.getElementById("NavBar__list");
+//const numberOfListItems = sections.length;
+// Construct the navbar
+
+function buildNav() {
+    for (let index = 0; index <Array.length; index++) {
+        const element = array[index];
+    }
+}
+//
+function buildNav() {
+    sections.forEach(section=>{
+        const navButton = document.createElement('li');
+        navButton.insertAdjacentHTML("afterBegin",`<a href="#${section.id}" class="menu__link">${section.dataset.nav}</a>`);
+        navListUL.appendChild(navButton);
+
+        scrollBehavior(navButton, section);
+    });
+
+    navBar.appendChild(navListUL);
+}
+
+buildNav();
+//
+listItem = document.createElement('li');
+
+navBar.appendChild(listItem);
+function scrollBehavior(navButton,section){
+    navButton.addEventListener('click', function(event){
+        event.preventDefault();
+        window.scrollTo({
+            top: section.offsetTop, behavior: "smooth"
+        })
+    })
+}
+
+//
+function activeSection(){
+    const navActive = document.querySelectorAll(".menu__link")
+    sections.forEach((section, i)=>{
+    const sectionBond = section.getBoundingClientRect();
+        if (sectionBond.top <= 380 && sectionBond.bottom >= 350){
+            section.classlist.add("your-active-class");
+            navACtive[i].classList.add(active_button);
+        } else{
+            section.classList.remove("your-active-class");
+            navActive[i].classList.remove("active_button")
+        }
+    })
+}
+function toggleNavBar(){}
+
+window.addEventListener('scroll', smoothScroll); {
+    function smoothScroll(){
+        sections.forEach(section =>{
+            const sectionTop = section.getBoundingClientRect().top;
+            const link = document.querySelector(`a[href = "#${section.id}"]`)
+        })
+
+    activeSection();
+    toggleNavBar();
+}
+};
 //below Function & for loop in order to assign name of each section into the array above.// 
-function createListItem(){
+/*function createListItem(){
     for (section of sections) {
         sectionName = section.getAttribute("data-nav");
         listItem = document.createElement("li");
