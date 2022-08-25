f48f8cd5a41bbc606a8c8e8df82c31104d0dc64b
YKTV8 <97923330+YKTV8@users.noreply.github.com>
2022-08-25 08:07:34 -0700

    08/25
    No errors, also no active section background on scroll.

--- marjon/js/app1.js
+++ marjon/js/app1.js
@@ -1,63 +1,61 @@
 //DEFINE GLOBAL VARIBLE
-const navBar = document.querySelector('.navbar__menu');
-const navListUL = document.querySelector('#navbar__list');
-//const paraElems = documents.querySelectorAll('p');
-const sections = document.querySelectorAll('section');
-let section = Array.from(document.querySelectorAll('section'));
 
+//NavBar selection & const sections [sec1 thru sec6]
+const navListUL = document.getElementById("navbar__list");
+
+const navBar = document.querySelector(".navbar__menu");
+//const navListUL = document.querySelector("#navbar__list");
+//const paraElems = documents.querySelectorAll('p');
+const sections = document.querySelectorAll("section");
+//let section = Array.from(document.querySelectorAll("section"));
+//const section = Array.from(sections);
 
 //CONSTRUCT NAVBAR DYNAMICALLY
 
-function buildNav() {
+/*function buildNav() {
     for (let index = 0; index <Array.length; index++) {
         const element = Array[index];
     };
+};*/
+const buildNav = () => {
+    let navbar__menu = '';
+
+    sections.forEach(section=>{
+        const navButton = document.createElement("li");
+        navButton.insertAdjacentHTML("afterBegin",`<a href="#${section.id}" class="menu__link">${section.dataset.nav}</a>`);
+        navListUL.appendChild(navButton);
+    });
+};
+//CONFIRM VIEWPORT AND CREATING ACTIVE CLASS
+
+const offset = (section) =>{
+    return Math.floor(section.getBoundingClientRect().top);
 };
 
-section.forEach(section=>{
-    const navButton = document.createElement('li');
-    navButton.insertAdjacentHTML("afterBegin",`<a href="#${section.id}" class="menu__link">${section.dataset.nav}</a>`);
-    navListUL.appendChild(navButton);
+const removeActiveSection = (section) => {
+    section.classList.remove('your-active-section');
+};
 
-    buildNav(navButton, section);
-});
-
-//navBar.appendChild(navListUL);
-
-window.addEventListener("scroll", function (z){
-section.forEach (function(section){
-    const topOfSect = section.getBoundingClientRect().top;
-
-    if(topOfSect => 150 && topOfSect <350){
-        section.classList.add('your-active-class');
-    } else { 
-        section.classList.remove('your-active-class');
-    }
-    });
-});
-/*
-//CONFIRM VIEWPORT AND CREATING ACTIVE CLASS
-const sectionInViewport = () =>{
-    const sections = sections.getBoundingClientRect();
-    return sections.top > 0 && sections.bottom > 550;
-};*/
+const addActiveSection = (conditional, section) => {
+    if(conditional){
+    section.classList.add('your-active-section');
+    };
+};
 
 const activeSection = () => {
-    for (let menu__link of sections){
-        if (sectionInViewport(sections)) {
-            if(!sections.classList.contains("your-active-class")) {
-                sections.classList.add("your-active-class");
-                sections.style.background.add(".active-section");
-            }
-      } else {
-        sections.classList.remove("your-active-class");
-      }
-    }
-};
+    sections.forEach(sections => {
+        const elementOffset = offset(sections);
 
+        inViewport = () => elementOffset < 0 && elementOffset >= -150;
+
+        removeActiveSection(sections);
+        addActiveSection(inViewport(),sections);
+        });
+};
+window.addEventListener("scroll", activeSection);
 //navBar.appendChild(navListUL);
-function scrollBehavoir(navButton,sections){
-    navButton,addEventListener('click', function(event){
+/**function scrollBehavoir(navButton,sections){
+    navButton,addEventListener("click", function(event){
         event.preventDefault();
         window.scrollIntoView({
             top: section.offsetTop,
@@ -70,8 +68,8 @@ function scrollBehavoir(navButton,sections){
 scrollTo();
 
 const smoothScroll = () =>{
-    document.querySelectorAll('.menu__link').forEach((anchor)=> {
-        anchor.addEventListener('scroll', function () {
+    document.querySelectorAll(".menu__link").forEach((anchor)=> {
+        anchor.addEventListener("scroll", function () {
             preventDefault ();
             document.querySelector(anchor.getAttribute("href")).scrollIntoView({
                 behavior: "smooth",
@@ -104,4 +102,4 @@ buildNav();
 
 //SMOOTH SCROLL TO SECTION
 
-smoothScroll();
+//smoothScroll();
