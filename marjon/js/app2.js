1a41838e6b8afb092515cb8d3e063ff5e08a7e49
Geoffrey Matthews <97923330+YKTV8@users.noreply.github.com>
2022-09-05 23:36:01 -0700

    09/05 updates
    miinor adjustments made getBoundingRect not working

--- marjon/js/app2.js
+++ marjon/js/app2.js
@@ -35,101 +35,30 @@ const activeSection = () => {
     sections.forEach(section => {
         const elementOffset = offset(section);
 
-        inViewport = () => elementOffset < 6 && elementOffset >= -850;
+        inViewport = () => elementOffset < 224 && elementOffset >= 224;
 
         removeActive(section);
         addActive(inViewport(),section);
         });
-};
-  // console.log(navBarUL);
-  window.addEventListener('click', scrollIntoView);
-  
-buildNav();
-//}
 
+window.addEventListener('scroll', () => {
+    sections.forEach( (section) => {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const navListUL = document.getElementById("navbar__list");
-const navBar = document.querySelector(".navbar__menu");
-
-//CONSTRUCTION NAVBAR
-function addActive(){
-    constAtags = document.getElementsByTagName('a')
-    for(let i = 0; i < section.length; i++){
-        section[i].classList.remove("your-active-class");
-        Atags[i].classList.remove("active");
-    }
-}
-for(let index = 0; index <Array.length; index++){
-    const element = Array[index];
-
-    const buildNav = () => {
-        let navbar__menu = '';
-
-        sections.forEach(section=>{
-            const navBar =document.createElement("li");
-            navBar.insertAdjacentHTML("afterBegin", `<a href="#${section.id}" class=menu__link">${section.dataset.nav}</a>`);
-            navListUL.appendChild(navBar);
-        });
-        navigation.innerHTML = navBar;
-    };
-    const offset = (section) =>{
-        return Math.floor(section.getBoundingClientRect().top);
-    };
-    
-    const removeActive = (section) => {
-        section.classList.remove('your-active-section');
-        section.style.cssText = "background-color: rgb(125, 0, 125);";
-    };
-    
-    const addActive = (conditional, section) => {
-        if(conditional){
-        section.classList.add('your-active-section');
-        section.style.cssText = "background-color: rgb(15, 200, 125);";
-        };
-    };
-    const activeSection = () => {
-        sections.forEach(section => {
-            const elementOffset = offset(section);
-    
-            inViewport = () => elementOffset < 6 && elementOffset >= -850;
-    
-            removeActive(section);
-            addActive(inViewport(),section);
-            });
-    };
-
-    navBar.addEventListener("click", function (event){
-        event.preventDefault();
-        //const activeSect = event.target.getAttribute("data-id");
-        const sectionToScrollTo = document.getElementById(sections.id);
-        addActive();
-        event.target.classList.add("active");
-        sectionToScrollTo.classList.add("your-active-class");
-        sectionToScrollTo.scrollIntoView({
-            behavior: 'smooth'
-        });
+      // getBoundingClientRect() method returns a DOMRect object providing information about the size of an element and its position relative to the viewport.
+      const inView = section.getBoundingClientRect();
+      const id = section.getAttribute('id');
+      const position =  inView.y <= 24 && inView.bottom >= 24
+      // section active control
+    console.log(inView);
     });
+})};
+/**function activeSection{
+    window.addEventListener('click', inViewport){
+    const inViewport = sections.getBoundingClientRect();
+    const id = sections.getAttribute('id');
+    const position = inView.y <= 6 && inView.bottom >= 6;**/
+  // console.log(navBarUL);
 
-    buildNav();
-}*/
+  // console.log(navBarUL);
+  //window.addEventListener('click', inViewport);
+buildNav();
