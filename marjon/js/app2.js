d834d89c534728efd777c37298dac8d5f5ca9542
Geoffrey Matthews <97923330+YKTV8@users.noreply.github.com>
2022-09-07 00:26:15 -0700

    Update app2.js
    Debugger added. no errors working on section background

--- marjon/js/app2.js
+++ marjon/js/app2.js
@@ -12,52 +12,34 @@ let section = document.querySelectorAll("section");
             navBar.innerHTML = `<a href="#${section.id}" class="menu__link">${section.dataset.nav}</a>`;
             
             navListUL.append(navBar);
+            navBar.addEventListener("click", ()=> {
+                section.scrollIntoView({behavior:"smooth"});
+            });
         });
     
       //  navigation.innerHTML = navBar;
     };
 
 buildNav();
-console.log(buildNav)
 
-
-navBar.addEventListener("click", intoView);
-
-function intoView (){
-    sections.scrollIntoView({behavoir : "smooth"});
-};
 
 //Scroll Detection for sections
 window.addEventListener("scroll", () =>{
     sections.forEach((section => {
         const inView = section.getBoundingClientRect();
         const id = section.getAttribute("id");
-        const position = inView.top <=175 && inView.bottom >= 175;
+        const position = inView.top <= 375 && inView.bottom >= 375;
+
+        const addActive = (conditional, section) => {
+            if(conditional){
+            section.classList.add('your-active-section');
+            section.style.cssText = "background-color: rgb(15, 200, 125);";
+            };
 
 const removeActive = (section) => {
     section.classList.remove('your-active-section');
     section.style.cssText = "background-color: rgb(125, 0, 125);";
-};
-
-const addActive = (conditional, section) => {
-    if(conditional){
-    section.classList.add('your-active-section');
-    section.style.cssText = "background-color: rgb(15, 200, 125);";
-    };
-};
-/*
-const activeSection = () => {
-    sections.forEach(section => {
-        const elementOffset = offset(section);
-
-        inViewport = () => elementOffset < 224 && elementOffset >= 224;
-
-        removeActive(section);
-        addActive(inViewport(),section);
-        });
-
-    };*/
-}));
+        }};
 
 window.addEventListener('scroll', () => {
     sections.forEach( (section) => {
@@ -67,9 +49,9 @@ window.addEventListener('scroll', () => {
       const id = section.getAttribute('id');
       const position =  inView.y <= 24 && inView.bottom >= 24
       // section active control
-    console.log(inView);
+    //console.log(inView);
     });
 });
 
-
-});
+console.log(scrollY)
+}))});
