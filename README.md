a514785a0f4595a8b0826ddd817ed0c87f331c8d
Geoffrey Matthews <97923330+YKTV8@users.noreply.github.com>
2022-09-10 18:21:33 -0700

    09/10
    Submitted code

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # Landing-Page-Final
+As known as the Marjon Project, has been contructed with html, designed with css and given interactive features in using JavaScript.
+
+###**Known Bugs** 
+1.Section in Nav bar and on scroll are iterated for the last section in HTML. 
+2.Section in navbar will move the screen to correct sections, navbar is not fixed on screen and is coded for double click in order to have background color change. 
+3.Code has been redacted - 
+
+
+###**Sources used for inspiration:**
+_jasonRete Cohort - After course meetups on Sundays_
+_George Ingram - Lead for thr full duration of the courses_
+_Rockwell- FEND Lead for Udacity_
+_Kathy Jones FEND lead for Udacity_
+_Sharynne FEND Lead for Udacity_
+_Earl Pearley Cohort- After course meetups on Sundays_
+_AhmeddSaid GitHub account_
+_Raymond Johnson Cohort_ 
+_Slack feed_
+_Google....plenty of google_
+_w3schools_
+_csscheatsheet.com (html, css, js)_
+
+
+##**Files to download**
+All files will be found within the `marjon`
+`html`
+`css folder`
+`javaScipt folder`
+`images folder`
+`LICENSE`
+`README.md`
+
+##**Functions You'll Find**
+`getSectionPositions`
+`activeSection`
+`buildNav`
+`trackScrolling`
+##**Event Listners You'll come across**
+`Click`
+`Scroll`
+
+##
+
+
+
--- marjon/js/app4.js
+++ marjon/js/app4.js
@@ -1,22 +1,32 @@
-//GLOBAL DEFINE
+/* Manipulating the DOM exercise.
+* Exercise programmatically builds navigation,
+* scrolls to anchors from navigation,
+* and highlights section in viewport upon scrolling.
+* 
+* Dependencies: None
+* 
+* JS Version: ES2015/ES6*/
+//GLOBAL DEFINE Section and navbar__list globally defined
+//Grabs unordered list from the page
 const sections = document.querySelectorAll('section');
+//Grabs sections from the page
 const navListUL = document.getElementById('navbar__list');
 //
-let topOfSection = [];
-//
+let topOfSections = [];
+//Section position - coded example from Rockwell. window.pageYOffset and if statement provided in walkthrough video
 function getSectionPositions(){
-    const footerTopPosition = document.querySelectorAll('footer') + window.pageYOffset;
+    const footerTopPosition = document.querySelectorAll('page__footer');
     for(let index = 0 ; index < sections.length; index++){
     let topPos = sections[index].getBoundingClientRect().top + window.pageYOffset;
-    topOfSection[index] = topPos;
+    topOfSections[index] = topPos;
     if(index + 1 == sections.length){
-        topOfSection[index + 1] = footerTopPosition;
+        topOfSections[index + 1] = footerTopPosition;
     };
     };
     
-    console.log(topOfSection);
+    console.log(topOfSections);
 };
-//construct navbar
+//for loop creating active section removes active section style prior to passing thru
 function activeSection(){
     for(let index = 0 ; index < sections.length; index++) {
         const anchorTags = document.getElementsByTagName('a');
@@ -24,6 +34,7 @@ function activeSection(){
         anchorTags[index].classList.remove('active');
     };
 };
+//For loop to dynamically building navbar using for loop. 
 function buildNav(){
     for(let index = 0 ; index < sections.length; index++) {
         const navMenu = document.getElementById('navbar__list');
@@ -31,18 +42,17 @@ function buildNav(){
         const aNavItem = document.createElement('a');
     
         const liLabel = sections[index].getAttribute('data-nav');
-        aNavItem.classList.add('anchor');
+        aNavItem.classList.add('anchorNav');
         aNavItem.innerHTML = liLabel;
-        liNavItem.classList.add('list');
-    
+        liNavItem.classList.add('listNav');
         aNavItem.setAttribute('href', '#'+ sections[index].id);
         aNavItem.setAttribute('data-id', sections[index].id);
-        //liNavItem.innerHTML = `<a class='menu__link'>${sections.[index].dataset}</a>`;
-    
+        //code below throws error in DOM
+        //liNavItem.innerHTML = `<a class='menu__link'>${sections.dataset}</a>`;
         liNavItem.append(aNavItem);
         navMenu.append(liNavItem);
-    
         //console.log(liNavItem)
+        //Click event listener add for activation of activeSection function of navbar
         aNavItem.addEventListener('click', function(event) {
             event.preventDefault();
             const sectionId = event.target.getAttribute('data-id');
@@ -56,74 +66,30 @@ function buildNav(){
         });
     };
 };
-/*for(let index = 0 ; index < sections.length; index++) {
-    const navMenu = document.getElementById('navbar__list');
-    const liNavItem = document.createElement('li');
-    const aNavItem = document.createElement('a');
-
-    const liLabel = sections[index].getAttribute('data-nav');
-    aNavItem.classList.add('anchor');
-    aNavItem.innerHTML = liLabel;
-    liNavItem.classList.add('list');
-
-    aNavItem.setAttribute('href', '#'+ sections[index].id);
-    aNavItem.setAttribute('data-id', sections[index].id);
-    //liNavItem.innerHTML = `<a class='menu__link'>${sections.[index].dataset}</a>`;
-
-    liNavItem.append(aNavItem);
-    navMenu.append(liNavItem);
-
-    //console.log(liNavItem)
-    aNavItem.addEventListener('click', function(event) {
-        event.preventDefault();
-        const sectionId = event.target.getAttribute('data-id');
-        const sectionInViewport = document.getElementById(sectionId);
-        activeSection();
-        event.target.classList.add('active');
-        sectionInViewport.classList.add('your-active-class');
-        sectionInViewport.scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-};*/
-
-
+//scrolling function created to give indication of when one section is active and another is inactive.
 function trackScrolling() {
+    const tagName = document.getElementsByTagName('a');
     const positionY = window.pageYOffset;
     for(let index = 0; index <sections.length; index++){
-        const firstPosition = topOfSection[index];
-        const secondPosition = topOfSection[index+1];
+        const firstPosition = topOfSections[index];
+        const secondPosition = topOfSections[index+1];
         if(firstPosition <= positionY && secondPosition > positionY){
             activeSection();
             sections[index].classList.add('your-active-class');
-            sections[index].classList.add('active');
+            tagName[index].classList.add('active');
         };
     };
+    console.log(positionY);
 };
-    /*    const inView = sections[index].getBoundingClientRect().top;
-        //const id = sections.getAttribute('id');
-        const positionY = window.pageYOffset;
-        sections.forEach(index => {
-            const firstPosition = topOfSection[index];
-            const secondPostion = topOfSection[index+1];
-            if(firstPosition <= inView && secondPosition) {
-                activeSection();
-                    sections[index].classList.add('your-active-class');
-                    anchorTags[index].classList.add('active');
-            }
-          })
-        //console.log(positionY);
-        };*/
 buildNav();
 getSectionPositions();
-
 window.addEventListener('scroll', trackScrolling);
 
 /*() =>{
     for(let index = 0 ; index < sections.length; index++) {
-        const inView = sections[index].getBoundingClientRect().top;
+        const inView = sections[index].getBoundingClientRect().bottom;
         const id = sections.getAttribute('id');
-        const positionY = inView.top <=463 && inView.bottom >= 463;
+        const positionY = inView.top <=463 && inView.top > 6748;
         if(positionY) {
             sections.classList.add("your-active-class");
             navListUL.querySelector(`[data-id=${id}]`).classList.add('your-active-section');
@@ -134,4 +100,6 @@ window.addEventListener('scroll', trackScrolling);
         };
     };
 };*/
-        console.log(scrollY);
+//console.log(scrollY);
+
+//End Main Functions
