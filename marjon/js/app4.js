a6dafc357e15d0984542d7887cb945ff0e97949b
Geoffrey Matthews <97923330+YKTV8@users.noreply.github.com>
2022-09-11 23:06:38 -0700

    09/11 2nd attempt
    Revised JS

--- marjon/js/app4.js
+++ marjon/js/app4.js
@@ -8,45 +8,23 @@
 * JS Version: ES2015/ES6*/
 //GLOBAL DEFINE Section and navbar__list globally defined
 //Grabs unordered list from the page
-const sections = document.querySelectorAll('section');
-//Grabs sections from the page
-const navBar = document.querySelector (".navbar__menu");
-const navListUL = document.getElementById('navbar__list');
-//
-let topOfSections = [];
-//Section position - coded example from Rockwell. window.pageYOffset and if statement provided in walkthrough video
-function getSectionPositions(){
-    const footerTopPosition = document.getElementsByClassName('page__footer');
-    for(let index = 0 ; index < sections.length; index++){
-    let topPos = sections[index].getBoundingClientRect().top + window.pageYOffset;
-    topOfSections[index] = topPos;
-    if(index + 1 == sections.length){
-        topOfSections[index + 1] = footerTopPosition;
-    };
-    };
-   // console.log(topOfSections);
-};
-//for loop creating active section removes active section style prior to passing thru
-function activeSection(){
-    for(let index = 0 ; index < sections.length; index++) {
-        const tagName = document.getElementsByTagName('a');
-        sections[index].classList.remove('your-active-class');
-        tagName[index].classList.remove('active');
-    };
-    //console.log(activeSection);
-};
+
+// Dynamically build the navigation menu
 //For loop to dynamically building navbar using for loop. 
+const sections = document.querySelectorAll('section');
+document.addEventListener('DOMContentLoaded', activeSection, false); 
+document.addEventListener('DOMContentLoaded', ActiveNavBar, false); 
 function buildNav(){
     
     for(let index = 0 ; index < sections.length; index++) {
         const navMenu = document.getElementById('navbar__list');
         const liNavItem = document.createElement('li');
         const aNavItem = document.createElement('a');
-    
+        //console.log(liNavItem)
         const liLabel = sections[index].getAttribute('data-nav');
         aNavItem.classList.add('anchorNav');
         aNavItem.innerHTML = liLabel;
-        liNavItem.classList.add('listNav');
+        //liNavItem.classList.add('listNav');
         aNavItem.setAttribute('href', '#'+ sections[index].id);
         aNavItem.setAttribute('data-id', sections[index].id);
         //code below throws error in DOM
@@ -64,44 +42,58 @@ function buildNav(){
             sectionInViewport.classList.add('your-active-class');
             sectionInViewport.scrollIntoView({
                 behavior: 'smooth'
-            });
+        });
         });
     };
 };
-//scrolling function created to give indication of when one section is active and another is inactive.
-function trackScrolling() {
-    const tagName = document.getElementsByTagName('a');
-    const positionY = window.pageYOffset;
-    for(let index = 0; index <sections.length; index++){
-        const firstPosition = topOfSections[index];
-        const secondPosition = topOfSections[index+1];
-        if(firstPosition <= positionY && secondPosition > positionY){
-            activeSection();
-            sections[index].classList.add('your-active-class');
-            tagName[index].classList.add('active');
-        };
-    };
-  //  console.log(trackScrolling);
+//Brings the current section to the TOP of inViewport
+function activeSection (){
+    window.addEventListener('scroll', function() {
+		const sections = document.getElementsByClassName('section');		
+		for (let index = 0; index < sections.length; index++) {
+			const position = sections[index].getBoundingClientRect();
+			const topPos = position.top;
+				if (topPos <= window.innerHeight/2){
+					let activeClass = document.getElementsByClassName("your-active-class");
+					activeClass[0].className = activeClass[0].className.replace (" your-active-class", "");
+					sections[index].className += " your-active-class";
+			
+					let currentSect = document.getElementsByClassName("active");  
+						if (currentSect.length > 0) {
+							currentSect[0].className = currentSect[0].className.replace(" active", "");
+						}
+			
+					let menuContainer = document.getElementById("navbar__list").querySelectorAll('li');
+					menuContainer[index].className = " active";
+				};
+		};
+	});
+};
+//Assigns 'active' state in nav bar when section is clicked or scrolled into viewport..
+function ActiveNavBar (){
+	let menuContainer = document.getElementById("navbar__list");
+	let menuBar = menuContainer.querySelectorAll('li');
+	for (let index = 0; index < sections.length; index++) {
+  		sections[index].addEventListener("click", function() {
+			let actSect = document.getElementsByClassName("active");  
+    		if (actSect.length > 0) {
+      			actSect[0].className = actSect[0].className.replace(" active", "");
+    		}else{
+                sections[index].classList.remove('your-active-class');
+                sections[index].classList.remove('active');
+            };
+ 		});
+	};
+};
+//scrolling event function created to give indication of when one section is active and another is inactive.
+function Scrolling (section1) {
+	const section = document.getElementById('section'+section1);
+	const positionY = section.offsetTop;
+	event.preventDefault();
+	window.scrollIntoView({
+		left: 0, 
+		top: positionY,
+		behavior: 'smooth'
+	});
 };
 buildNav();
-getSectionPositions();
-window.addEventListener('scroll', trackScrolling());
-
-/*() =>{
-    for(let index = 0 ; index < sections.length; index++) {
-        const inView = sections[index].getBoundingClientRect().bottom;
-        const id = sections.getAttribute('id');
-        const positionY = inView.top <=463 && inView.top > 6748;
-        if(positionY) {
-            sections.classList.add("your-active-class");
-            navListUL.querySelector(`[data-id=${id}]`).classList.add('your-active-section');
-        } else{
-            sections.classList.remove("your-active-class");
-            navListUL.querySelector(`[data-id=${id}]`).classList.remove('your-active-section');
-
-        };
-    };
-};*/
-//console.log(scrollY);
-
-//End Main Functions
