e9d7f71ab7abdb2e2de86ab2469ad36abbaa9152
Geoffrey Matthews <97923330+YKTV8@users.noreply.github.com>
2022-09-14 22:38:54 -0700

    late push
    late push

--- starter_code/js/app1.js
+++ starter_code/js/app1.js
@@ -24,11 +24,17 @@
 */
 //**SOURCE https://www.encodedna.com/javascript/create-ul-and-li-elements-dynamically-using-javascript.htm */
 //**SOURCE https://stackoverflow.com/questions/69917843/using-foreach-method-with-array-to-build-navigation-menu-and-unordered-list-with */
-const sections = document.querySelectorAll('section');
-const navBar = document.getElementById('navbar__list');
-//const nameId = document.getElementBy('id');
+const sectionsBethany = document.querySelectorAll('section');
+const navBarOne = document.getElementById('navbar__list');
 const arraySections = document.querySelectorAll('section');
-const navBartoggler = document.getElementsByClassName('page__header');
+const boxedSection = document.querySelector("div.section");
+const result = boxedSection.getBoundingClientRect();
+//console.log(result);
+// unused variable const navBarToggle = document.getElementsByClassName('page__header');
+const viewablePosition = (pos) => {
+    let sectionPos = pos.getBoundingClientRect();
+    return sectionPos.top <=1000 && sectionPos.bottom >= 1000;
+};
 /**
  * End Global Variables
  * Begin Main Functions
@@ -44,7 +50,7 @@ function buildDynamicNav () {
     sectionId = arraySections.getAttribute('id');
     //li and array id are assigned
     sectionName = arraySections.getAttribute('data-nav');
-    aNavBarhref.className = "menu";
+    aNavBarhref.className = "menu__link";
     //
     let itemName =`${sectionName}`;
     let idLink = `#${sectionId}`;
@@ -53,7 +59,7 @@ function buildDynamicNav () {
     aNavBarhref.dataset.nav = sectionId;
     aNavBarhref.href= idLink;
     listNavBar.append(aNavBarhref);
-    navBar.append(listNavBar);
+    navBarOne.append(listNavBar);
 //^^^navbar appended
     listNavBar.addEventListener("click", () => {
         arraySections.scrollIntoView({
@@ -62,60 +68,65 @@ function buildDynamicNav () {
     })
  }
  )};
+ 
+ buildDynamicNav();
 //SOURCE:https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
 //getBoundingclientrect
-function windowPosition (top) {
+/*function windowPosition (top) {
     const winPos = top.getBoundingClientRect();
-    return winPos.top <= 425 && winPos.bottom >= 425;
+    return winPos.top <= 412 && winPos.bottom >= 412;
     if(winPos.top) {
-        sections.classList.add('your-active-class');
-        navBar.querySelector(`[data-id=${id}]`).classList.add('your-active-class');
-    }else{sections.classList.remove('your-active-class');
-    navBar.querySelector(`[data-id=${id}]`).classList.remove('your-active-class');}
-};
-//
-//function navBarClick("click", (event){
-//    listNavBar.classList.toggle("navbar__menu");
+        sectionsBethany.classList.add('your-active-class');
+        navBarOne.querySelector(`[data-id=${id}]`).classList.add('your-active-class');
+    }else{sectionsBethany.classList.remove('your-active-class');
+    navBarOne.querySelector(`[data-id=${id}]`).classList.remove('your-active-class');}
+};*/
 
 // source https://www.w3schools.com/howto/howto_js_active_element.asp
 // Add class 'active' to section when near top of viewport
 //Add your-active-section
-function activateCurrentSection () {
-    sections.forEach(sections => {
-        const activeView = navBar.querySelector(
-            `[data-nav=${sections.id}]`
+const activateCurrentSection = () => {
+    sectionsBethany.forEach(sectionsBethany => {
+        const activeView = navBarOne.querySelector(
+            `[data-nav=${sectionsBethany.id}]`
         );
         //When section appears within viewport, windowPostion reflect 'active'
         //** if,else statement SOURCE: https://www.w3schools.com/jsref/jsref_if.asp
         //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else
-        if(windowPosition(sections)){
+        if(viewablePosition(sectionsBethany)){
             sections.classList.add('your-active-class');
             activeView.classList.add('active');
             //When section appears outside of the viewport, windowPosition will have the 'active' removed.
         }else{
-            sections.classList.remove('your-active-class');
+            sectionsBethany.classList.remove('your-active-class');
             activeView.classList.remove('active');
             //remove your-active-class
         } 
     });
 }
 
-buildDynamicNav();
+const scrolling = () => {
+    const menuLinkTod = document.querySelectorAll(".menu__link");
+    menuLinkTod.forEach((id)=> {
+        id.addEventListener("click", (scrolling)=> {
+            scrolling.preventDefault();
+            const anchorId = document.querySelector(id.getAttribute("href"));
+            anchorId.scrollIntoView({
+                behavior: "smooth"
+            });
+        });
+    });
+};
+/*
+/*buildDynamicNav();
 smoothScroll();
 activateCurrentSection();
-//ACtive Section Manager function - Used to clear all active sections prior to being target called
+//ACtive Section Manager function - Used to clear all active sections prior to being target called*/
 
 //**SOURCE: https://www.delftstack.com/howto/javascript/scrollevent-in-javscript/#:~:text=We%20can%20use%20scroll%20event%20Listeners%20to%20listen,console.log%28scrollX%29%3B%20%7D%29%3B%20The%20this%20signifies%20the%20window%20object.
 window.addEventListener("scroll", smoothScroll => {
     let scrollY = this.scrollY;
     console.log(scrollY);
 });
-//**SOURCE https://javascript.info/onscroll */
-window.addEventListener('scroll', activateCurrentSection);
-function smoothScroll(event){
-    window.scrollTo({
-        top: activateCurrentSection,
-        behavior:'smooth'
-    })
-};
-//window.addEventListener("click", navBarClick)
+//**SOURCE https://javascript.info/onscroll
+scrolling();
